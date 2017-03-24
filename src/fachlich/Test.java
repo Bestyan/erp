a394d2fa34c66b0b503c84da9d3b070fd982d5f4
SchatteB <SchatteB@10.1.20.61>
2017-03-24 12:24:04 +0100

    first test

--- src/fachlich/Test.java
+++ src/fachlich/Test.java
@@ -2,14 +2,24 @@ package fachlich;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import fachlich.BapiFactory.BapiType;
 
 public class Test {
 	public static void main(String [] args)
 	{
 		SapModel model = new SapModel("915", "GBI-0-18", "123456");
-		HashMap<String, Object> parameters = new HashMap<>();
-		ArrayList<HashMap<String, String>> matnrSelection = new ArrayList<>();
-		
+		Map<String, Object> parameters = new HashMap<>();
+		List<Map<String, String>> matnrSelection = new ArrayList<>();
+		Map<String, String> row = new HashMap<>();
+		row.put("SIGN", "I");
+		row.put("OPTION", "CP");
+		row.put("MATNR_LOW", "ERPKÖ*");
+		matnrSelection.add(row);
+		parameters.put("MATNRSELECTION", matnrSelection);
+		Map<String, Object> result = model.executeBapi(BapiType.GETLIST, parameters);
 		
 	}
 }
